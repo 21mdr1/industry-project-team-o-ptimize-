785878942e7a6d986815f3b76db7c41f4e0ce985
stevefali <stevefaliszewski@gmail.com>
2024-01-26 08:05:59 -0800

    resolve merge conflict

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
+import ChatBox from "../../components/ChatBox/ChatBox";
 import "./Home.scss";
 
 function Home() {
-  return <main className="main--home">Home</main>;
-=======
-import ChatBox from '../../components/ChatBox/ChatBox';
-import './Home.scss';
-
-function Home() {
-    return (
-        <>
-            <main className="main--home">Home</main>
-            <ChatBox />
-        </>
-    );
->>>>>>> develop
+  return (
+    <>
+      <main className="main--home">Home</main>
+      <ChatBox />
+    </>
+  );
 }
 
 export default Home;
