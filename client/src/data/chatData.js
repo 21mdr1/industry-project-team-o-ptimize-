3baecd658651dad1fc98a96b51a7af966e287ec7
stevefali <stevefaliszewski@gmail.com>
2024-01-26 10:42:26 -0800

    Start chat data array

---
+++ client/src/data/chatData.js
@@ -0,0 +1,74 @@
+const chat = [
+  {
+    name: "Mia",
+    avatar: "bot-image.jpg",
+    comments: {
+      comment: "Hello! \nWhat can I help you with today?",
+      comment: "Suggested topics: ",
+    },
+    buttons: {
+      button: "End Chat",
+      button: "Family Vacations",
+      button: "Best Places to Visit",
+      button: "Our packages",
+      button: "Best Deals",
+      button: "Popular Destinations",
+      button: "Weekend Getaways",
+      button: "Europe Cruises",
+      button: "Caribbean Cruises",
+      button: "Alaska Cruises",
+    },
+  },
+  {
+    name: "Mia",
+    avatar: "",
+    comments: {
+      comment: "Great! \nDo you have any questions for me?",
+    },
+    buttons: "",
+  },
+  {
+    name: "You",
+    avatar: "",
+    comments: {
+      comment:
+        "Hello, \nI want to book a cruise for my family and we were thinking of going around the 15th or the 31st of May on the West Caribbean cruise.",
+    },
+    buttons: "",
+  },
+  {
+    name: "Mia",
+    avatar: "bot-image.jpg",
+    comments: {
+      comment:
+        "You chose an excellent time! \nLet me ask you some questions regarding the type of trip you want.",
+      comment:
+        "How many adults and how many children will be going on the cruise?",
+    },
+    buttons: "",
+  },
+  {
+    name: "You",
+    avatar: "",
+    comments: {
+      comment:
+        "It’s going to be my wife and I, and our two children 9m and 11m.",
+    },
+    buttons: "",
+  },
+  {
+    name: "Mia",
+    avatar: "bot-image.jpg",
+    comments: {
+      comment:
+        "We have a lot of options for you. \n \nBefore I send offers your way is there anything you would like to ask me? Anything we can cover to make you feel comfortable and secure?",
+      comment: "Suggested topics:",
+    },
+    buttons: {
+      button: "Child Safety",
+      button: "Cleanliness",
+      button: "Best Deals",
+      button: "Our Packages",
+    },
+  },
+];
