0f1e5ff5bfcc1d90df87138d1575a6383737fd07
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-26 11:45:23 -0500

    Style open button

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,14 +1,16 @@
 import ChatBox from '../../components/ChatBox/ChatBox';
-import close from '../../assets/icons/close-icon-white.svg'
+import open from '../../assets/icons/entypo_chat.svg';
+import close from '../../assets/icons/close-icon-white.svg';
 import './Home.scss';
 
 function Home() {
+
     return (
         <>
             <main className="main--home">Home</main>
             <ChatBox className="chat-box--home"/>
             <button type="button" className="chat-box__open-button">
-              <img src={close} alt="close chat" />
+              <img src={open} alt="close chat" className='chat-box__open-image' />
             </button>
         </>
     );
